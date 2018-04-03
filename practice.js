b2f5a8876227e744b4dabbe83267eb1223ade7a0
Imron Khatib <imronskhatib@gmail.com>
2018-04-02 21:22:12 -0500

    attempt for loop

---
+++ practice.js
@@ -0,0 +1,20 @@
+// # Write a method that prints out every number from 1 to 100. 
+// var numberArray = [];
+// var number = 0;
+// for (var i = 0; numberArray.length; i++){
+//   numberArray << number++;
+// }
+// console.log(numberArray);
+
+// var numberArray = [];
+// var number = 0;
+// for (var i = 0; i <= 100; i++) {
+//   console.log(i);
+// }
+
+// // console.log(numberArray);
+
+var numberArray = [1,2,3,4,5,6,7,8,9,10];
+for (var i++; i % 2 === 0;i++) {
+  console.log(i);
+}
