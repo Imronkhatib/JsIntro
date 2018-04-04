8f9d42c98733e2699ce73646cfb55aa6f7458294
Imron Khatib <imronskhatib@gmail.com>
2018-04-03 21:09:28 -0500

    Create a function that accepts an array of strings and returns a new array, function that prints max number from array, function that provides factorial of input

--- practice.js
+++ practice.js
@@ -14,7 +14,123 @@
 
 // // console.log(numberArray);
 
-var numberArray = [1,2,3,4,5,6,7,8,9,10];
-for (var i++; i % 2 === 0;i++) {
-  console.log(i);
-}
+// var numberArray = [1,2,3,4,5,6,7,8,9,10];
+// for (var i++; i % 2 === 0;i++) {
+//   console.log(i);
+// }
+
+
+
+// Equality check
+// x = 10
+// if x == "10"
+//   puts "equal"
+// else
+//   puts "not equal"
+// end
+
+// var x = 11;
+// if (x === 10){
+//   console.log("equal");
+// }
+
+// else {
+//   console.log("not equal");
+// }
+
+// var x = false;
+// if (null){
+//     console.log("x = x + 1");
+//   }
+  
+// Write a function that accepts an array of strings and returns a new array containing every other string from the original array. For example, if the input is [“a”, “b”, “c”, “d”, “e”, “f”], the output should be [“a”, “c”, “e”].
+
+// function everyOtherString(inputArray) {
+//   var newArray = [];
+//   for (var i = 0; i < inputArray.length; i+=2) {
+//     newArray.push(inputArray[i]);
+//   }
+//   return newArray;
+// }
+
+// console.log(everyOtherString(["a", "b", "c", "d", "e", "f"]));
+
+
+// function greatestNumberGenerator(inputArray) {
+//   // var smallerArray = inputArray;
+//   var greatestNumber = 0;
+//   for (var i = 0, j = 1; i < inputArray.length; i++, j++) {
+//     if (inputArray[j] > inputArray[i]) {
+//       greatestNumber = inputArray[j];
+//     }
+//   }
+//   return greatestNumber;
+// }
+
+// console.log(greatestNumberGenerator([1,2,3,4,5,6,7,89,200000]));
+
+
+// Write a method that accepts a number and returns its factorial. For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
+
+// function fractional(number) {
+//   var total = 1;
+//   for (var i = number; i >= 1; i--) {
+//     // console.log(i);
+//     if (number >= 0) {
+//       total = total * number;
+//       number--;  
+//     }
+//   }
+//   return total;
+// }
+
+// console.log(fractional(5));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
